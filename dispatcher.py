9cf6059028131996dc1cb53227ed3d902a5b813f
MarkDziamentsyeu <96028827+MarkDziamentsyeu@users.noreply.github.com>
2022-12-21 14:16:10 +0300

    Update dispatcher.py

--- dispatcher.py
+++ dispatcher.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher,types
 
 
 
-bot = Bot(token = '5882446654:AAEM1apA2TAa3nrPWclPMDXUWeY-h73wV9s', parse_mode=types.ParseMode.HTML)
+bot = Bot(token = 'TOKEN', parse_mode=types.ParseMode.HTML) 
 dp = Dispatcher(bot)
 
 
